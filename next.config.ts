28b69763f451672fe001add47dbee20ba005b986
CommanderRedYT <ccommanderred@gmail.com>
2025-01-08 13:15:06 +0100

    Fix prisma

--- next.config.ts
+++ next.config.ts
@@ -2,53 +2,70 @@
 
 import type { NextConfig } from 'next';
 
+import { createRequire } from 'module';
+
 import { apiBaseUrl } from './src/constants';
 
-const nextConfig: NextConfig = {
-    /* config options here */
-    reactStrictMode: true,
-    logging: {
-        fetches: {
-            fullUrl: true,
+import { nodeFileTrace } from '@vercel/nft';
+
+const require = createRequire(import.meta.url);
+
+const nextConfig = async (): Promise<NextConfig> => {
+    const { fileList: additionalTracedFiles } = await nodeFileTrace([
+        // prisma.io
+        require.resolve('@prisma/client'),
+        require.resolve('prisma'),
+    ]);
+
+    return {
+        /* config options here */
+        reactStrictMode: true,
+        logging: {
+            fetches: {
+                fullUrl: true,
+            },
         },
-    },
-    env: {
-        API_BASE_URL: apiBaseUrl,
-    },
-    // TODO: Try to enable standalone when everything else works
-    output: 'standalone',
-    devIndicators: {
-        appIsrStatus: true,
-        buildActivity: true,
-        buildActivityPosition: 'bottom-right',
-    },
-    compiler:
-        process.env.NODE_ENV === 'production'
-            ? {
-                  removeConsole: {
-                      exclude: ['dir'],
-                  },
-                  reactRemoveProperties: true,
-              }
-            : {},
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: `${apiBaseUrl}/:path*`,
-            },
-        ];
-    },
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'static.media.ccc.de',
-                port: '',
-                search: '',
-            },
-        ],
-    },
+        env: {
+            API_BASE_URL: apiBaseUrl,
+        },
+        // TODO: Try to enable standalone when everything else works
+        output: 'standalone',
+        devIndicators: {
+            appIsrStatus: true,
+            buildActivity: true,
+            buildActivityPosition: 'bottom-right',
+        },
+        outputFileTracingIncludes: {
+            '**': [...additionalTracedFiles, './node_modules/.bin/prisma'],
+        },
+        compiler:
+            process.env.NODE_ENV === 'production'
+                ? {
+                      removeConsole: {
+                          exclude: ['dir'],
+                      },
+                      reactRemoveProperties: true,
+                  }
+                : {},
+        async rewrites() {
+            return [
+                {
+                    source: '/api/:path*',
+                    destination: `${apiBaseUrl}/:path*`,
+                },
+            ];
+        },
+        images: {
+            remotePatterns: [
+                {
+                    protocol: 'https',
+                    hostname: 'static.media.ccc.de',
+                    port: '',
+                    search: '',
+                },
+            ],
+        },
+    };
 };
 
 export default nextConfig;
