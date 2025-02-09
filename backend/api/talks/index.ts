3652f1d342428b3680b6e1e872dd1536766af3b2
CommanderRedYT <ccommanderred@gmail.com>
2025-02-10 00:58:55 +0100

    Fix linting

--- backend/api/talks/index.ts
+++ backend/api/talks/index.ts
@@ -3,13 +3,13 @@ import express from 'express';
 import handleAddEventRequest from '@backend/api/talks/add';
 import handleDeleteEventRequest from '@backend/api/talks/delete';
 import handleGetEventRequest from '@backend/api/talks/get';
+import handleImportJsonRequest from '@backend/api/talks/import';
 import handleVerifyJsonRequest from '@backend/api/talks/importVerify';
 import handleEventInfoRequest from '@backend/api/talks/info';
 import handleListEventsRequest from '@backend/api/talks/list';
 import handleScanEventsRequest from '@backend/api/talks/scan';
 import handleSearchEventsRequest from '@backend/api/talks/search';
 import handleUpdateEventRequest from '@backend/api/talks/update';
-import handleImportJsonRequest from '@backend/api/talks/import';
 
 const router = express.Router();
 
