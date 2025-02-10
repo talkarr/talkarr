39acbbc94dca9bf2e9002e9fdf858eb76b1d248a
CommanderRedYT <ccommanderred@gmail.com>
2025-02-10 23:29:54 +0100

    Do not fail directly when scanForMissingFiles throws

--- backend/workers/scanForMissingFiles.ts
+++ backend/workers/scanForMissingFiles.ts
@@ -44,84 +44,107 @@ const scanForMissingFiles: TaskFunction<ScanForMissingFilesData> = async (
 
     const events = job.data.event ? [job.data.event] : await listEvents();
 
+    let hasErrored = false;
+
     for await (const event of events) {
-        if (event.root_folder.did_not_find_mark) {
-            log.warn('Root folder mark was not found', {
+        try {
+            if (event.root_folder.did_not_find_mark) {
+                log.warn('Root folder mark was not found', {
+                    title: event.title,
+                    rootFolder: event.root_folder,
+                });
+
+                continue;
+            }
+
+            const hasFiles = await doesTalkHaveExistingFiles({ event });
+
+            const hasNfo = await doesEventHaveNfoFile({ event });
+
+            log.info(
+                `${event.title} ${hasFiles ? 'has files' : 'is missing files'}`,
+            );
+
+            const result = await createNewTalkInfo({ talk: event });
+
+            if (!result) {
+                log.error('Error creating new talk info:', {
+                    title: event.title,
+                });
+                continue;
+            } else {
+                log.info('Created new talk info:', { title: event.title });
+            }
+
+            if (!hasFiles) {
+                startAddTalk({ event });
+            } else {
+                await setIsDownloading({
+                    eventGuid: event.guid,
+                    isDownloading: false,
+                });
+
+                for await (const file of hasFiles) {
+                    const fileIsInDb = await checkIfFileIsInDb({
+                        eventGuid: event.guid,
+                        path: file.path,
+                    });
+
+                    log.info('Found file', {
+                        title: event.title,
+                        file,
+                        fileIsInDb,
+                    });
+
+                    if (!fileIsInDb) {
+                        const addFileToDbResult = await addDownloadedFile({
+                            event,
+                            file: {
+                                path: file.path,
+                                filename: pathUtils.basename(file.path),
+                                url: event.frontend_link,
+                                created: file.createdAt,
+                                mime: file.mime,
+                                bytes: file.size,
+                                is_video: file.isVideo,
+                            },
+                        });
+
+                        if (!addFileToDbResult) {
+                            log.error('Error adding file to db:', {
+                                title: event.title,
+                            });
+
+                            throw new Error('Error adding file to db');
+                        }
+                    }
+                }
+            }
+
+            if (!hasNfo) {
+                startGenerateMissingNfo({ event });
+            }
+        } catch (error) {
+            log.error('Error scanning for missing files:', {
+                error,
                 title: event.title,
-                rootFolder: event.root_folder,
             });
 
-            continue;
-        }
-
-        const hasFiles = await doesTalkHaveExistingFiles({ event });
-
-        const hasNfo = await doesEventHaveNfoFile({ event });
-
-        log.info(
-            `${event.title} ${hasFiles ? 'has files' : 'is missing files'}`,
-        );
-
-        const result = await createNewTalkInfo({ talk: event });
-
-        if (!result) {
-            log.error('Error creating new talk info:', { title: event.title });
-            continue;
-        } else {
-            log.info('Created new talk info:', { title: event.title });
-        }
-
-        if (!hasFiles) {
-            startAddTalk({ event });
-        } else {
             await setIsDownloading({
                 eventGuid: event.guid,
                 isDownloading: false,
             });
 
-            for await (const file of hasFiles) {
-                const fileIsInDb = await checkIfFileIsInDb({
-                    eventGuid: event.guid,
-                    path: file.path,
-                });
-
-                log.info('Found file', {
-                    title: event.title,
-                    file,
-                    fileIsInDb,
-                });
-
-                if (!fileIsInDb) {
-                    const addFileToDbResult = await addDownloadedFile({
-                        event,
-                        file: {
-                            path: file.path,
-                            filename: pathUtils.basename(file.path),
-                            url: event.frontend_link,
-                            created: file.createdAt,
-                            mime: file.mime,
-                            bytes: file.size,
-                            is_video: file.isVideo,
-                        },
-                    });
-
-                    if (!addFileToDbResult) {
-                        log.error('Error adding file to db:', {
-                            title: event.title,
-                        });
-
-                        throw new Error('Error adding file to db');
-                    }
-                }
-            }
-        }
-
-        if (!hasNfo) {
-            startGenerateMissingNfo({ event });
+            hasErrored = true;
         }
     }
 
-    done();
+    if (hasErrored) {
+        log.error('Error scanning for missing files');
+        done(new Error('Error scanning for missing files'));
+    } else {
+        done();
+    }
 };
 
 queue.process(taskName, 1, scanForMissingFiles);
