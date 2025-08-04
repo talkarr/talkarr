ae4048bf000450a60f938d95bf3ab6c80d5870bc
CommanderRedYT <ccommanderred@gmail.com>
2025-08-04 07:57:43 +0200

    Fix bug where tagged versions show unknown instead of the tag

--- backend/env.ts
+++ backend/env.ts
@@ -58,7 +58,7 @@ export const initEnv = async (): Promise<void> => {
         ]);
         privateGitDescribeResult = privateGitDescribeResult.trim();
 
-        process.env.NEXT_PUBLIC_CURRENT_VERSION = privateGitDescribeResult;
+        process.env.NEXT_PUBLIC_CURRENT_VERSION = getAppVersion();
     } catch (error) {
         console.error('Failed to get git commit hash:', error);
         privateGitDescribeResult = undefined;
